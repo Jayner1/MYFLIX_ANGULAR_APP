e44405ce63fb421cac89cf1ba5904ebd002dfe9f
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2023-02-15 09:24:56 -0800

    6.3

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from "rxjs";
 import { map } from "rxjs/operators";
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://myflix14.netlify.app/';
+const apiUrl = 'https://movie-api-08ww.onrender.com';
 @Injectable({
   providedIn: 'root'
 })

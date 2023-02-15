a21eadc12548d8b718c213b843299104f5171af7
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2023-02-15 15:05:41 -0800

    6.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ export class AppComponent {
 
   openMoviesDialog(): void {
     this.dialog.open(MovieCardComponent, {
-      width: '500px'
+      width: '280px'
     });
   }
 }
--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -14,16 +14,16 @@ export class FetchApiDataService {
   constructor(private http: HttpClient) {}
 
   userRegistration(userDetails: any): Observable<any> {
-    console.log(userDetails);
+    console.log('userRegistration() in service: ', userDetails);
     return this.http
-      .post(`${apiUrl}/users`, userDetails)
+      .post(apiUrl + 'users', userDetails)
       .pipe(catchError(this.handleError));
   }
 
+  //User login call
   userLogin(userDetails: any): Observable<any> {
-    console.log(userDetails);
     return this.http
-      .post(`${apiUrl}/login`, userDetails)
+      .post(apiUrl + 'login', userDetails)
       .pipe(catchError(this.handleError));
   }
 
